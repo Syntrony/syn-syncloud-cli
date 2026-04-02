9f2d866402329126b1c621578979f5c12770bc7c
JahirLopez1396 <jahir.ilv13@gmail.com>
2026-04-02 04:38:47 +0000

    Fix: Ajustes en log de informacion

--- cmd/root.go
+++ cmd/root.go
@@ -8,13 +8,33 @@ import (
 	"synctl/cmd/inspect"
 	"synctl/cmd/install"
 	"synctl/cmd/version"
+	"synctl/internal/logger"
 
 	"github.com/spf13/cobra"
 )
 
+var log *logger.Logger
+
+func preRun(cmd *cobra.Command, args []string) {
+	log = logger.NewConsoleLogger()
+	log.Command(">>> Running: synctl " + cmd.Name())
+}
+
+func postRun(cmd *cobra.Command, args []string) {
+	log.Command("<<< Completed: synctl " + cmd.Name())
+	log = nil
+}
+
 var RootCmd = &cobra.Command{
 	Use:   "synctl",
 	Short: "synctl is a CLI tool for managing syncloud resources",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		preRun(cmd, args)
+		return nil
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		postRun(cmd, args)
+	},
 }
 
 func init() {
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,21 +1,41 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-type Logger struct{}
+type Logger struct {
+	enableTimestamp bool
+}
 
 func NewConsoleLogger() *Logger {
-	return &Logger{}
+	return &Logger{enableTimestamp: true}
+}
+
+func (l *Logger) format(msg string) string {
+	if l.enableTimestamp {
+		return fmt.Sprintf("[%s] %s", time.Now().Format("2006-01-02 15:04:05"), msg)
+	}
+	return msg
 }
 
 func (l *Logger) Info(msg string) {
-	fmt.Println("[INFO]", msg)
+	fmt.Println("[INFO]", l.format(msg))
 }
 
 func (l *Logger) Debug(msg string) {
-	fmt.Println("[DEBUG]", msg)
+	fmt.Println("[DEBUG]", l.format(msg))
 }
 
 func (l *Logger) Error(msg string) {
-	fmt.Println("[ERROR]", msg)
+	fmt.Println("[ERROR]", l.format(msg))
+}
+
+func (l *Logger) Command(msg string) {
+	fmt.Println("[CMD]", msg)
+}
+
+func (l *Logger) Success(msg string) {
+	fmt.Println("[OK]", msg)
 }
